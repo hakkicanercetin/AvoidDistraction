57c6ac5b3e17394f1ae78bf3e79b08a2a1c862b7
Hakkıcan ERÇETİN <38086532+hakkicanercetin@users.noreply.github.com>
2023-11-10 05:39:47 +0300

    Add files via upload

---
+++ Javascript-Exercise-1/main.js
@@ -0,0 +1,12 @@
+function myFunction()
+{
+    let span =document.querySelector("span");
+    let sayac=Number(span.innerHTML);
+    sayac += 1;
+    span.innerHTML=sayac;
+    console.log(sayac)
+}
+function myFunction2()
+{
+    alert("Lo-fi müzik, rahatlatıcı ve hipnotik özellikleriyle bilinen bir tür düşük tempolu hip-hop müziğidir. Beynin rahatlamasına ve odaklanmasına yardımcı olabileceğinden, ders çalışmak isteyen öğrenciler için popüler bir seçenektir.")
+}
